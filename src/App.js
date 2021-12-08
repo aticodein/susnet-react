8587ce7945be632b0bb6e0b618f00697bd095f28
Attila Szaloki <szalokiattila@gmail.com>
2021-12-08 15:37:08 +0000

    first commit  react app created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>This is the React app I use for Tines Suset API data</h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tines.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tines
         </a>
       </header>
     </div>
